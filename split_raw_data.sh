27f1e62d21cfce78dc4a3e042f9aceae10ed4303
LohmannJens <jens.johann@outlook.de>
2023-10-10 17:24:38 +0200

    Added script to split large raw datasets

---
+++ split_raw_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+experiment=test
+accnum=SRR10489474
+
+input_file=data/raw_fastq/${experiment}/${accnum}
+
+outdir=data/raw_fastq/${experiment}/split
+
+zcat ${input_file}.fastq.gz | split -l 100000 - ${accnum}_part_
+
+for file in ${accnum}_part_*; do gzip "$file"; done
+
+if [ ! -d ${outdir} ]; then
+    mkdir -p ${outdir}
+fi
+
+mv ${accnum}_part_* ${outdir}
+
